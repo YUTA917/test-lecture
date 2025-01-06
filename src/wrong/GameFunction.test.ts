74b823b729adb83535e8ccd12c39470ec906a2af
itakura-shouho <shoho.0420@gmail.com>
2025-01-06 14:24:47 +0900

    READ MEを追加した

--- src/wrong/GameFunction.test.ts
+++ src/wrong/GameFunction.test.ts
@@ -2,8 +2,11 @@ import {play} from "./GameFunction";
 import { describe, it, expect } from 'vitest';
 
 describe("じゃんけん", () => {
+  //テストコードを見直して正しく書き換えてみましょう
   it("p1👊 vs p2✌️のときp1_winsが返る" , () => {
     const result = play()
     expect(result).toBe("p1-win",)
   })
+
+  //他の組み合わせのテストを記述してみましょう
 })
--- src/wrong/GameFunction.ts
+++ src/wrong/GameFunction.ts
@@ -2,5 +2,6 @@ type GameResult =  "p1-win" | "p2-win" | "Draw"
 
 type Throw = string //もっと限定的にしてください
 
+//じゃんけんの結果を返すplay関数を実装してみましょう
 export const play = () => {
 }
